fa1d358514a948d4322a2624ed991902a40c6b9b
Stefan Gustafsson <Stefangustaf@live.se>
2017-01-23 13:57:18 +0100

    Moved code from MainActivity to CreateUserActivity

---
+++ app/src/main/java/com/fatless/fatless/CreateUserActivity.java
@@ -0,0 +1,73 @@
+package com.fatless.fatless;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    private
+    @BindView(R2.id.editTextEmail)
+    EditText editTextEmail;
+    private
+    @BindView(R2.id.editTextPassword)
+    EditText editTextPassword;
+    private
+    @BindView(R2.id.buttonRegister)
+    Button buttonRegister;
+    private
+    @BindView(R2.id.textViewSignin)
+    TextView textViewSignin;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = CreateUserActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+}
