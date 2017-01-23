654ad16680ebc65e3eb9a43282bd4f32caa00829
jelloelks <kole.lleshaj@gmail.com>
2017-01-23 11:25:56 +0100

    Formulär skapat

--- app/src/main/java/com/fatless/fatless/MainActivity.java
+++ app/src/main/java/com/fatless/fatless/MainActivity.java
@@ -2,12 +2,40 @@ package com.fatless.fatless;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private
+    @BindView(R2.id.editTextEmail)
+    EditText editTextEmail;
+    private
+    @BindView(R2.id.editTextPassword)
+    EditText editTextPassword;
+    private
+    @BindView(R2.id.buttonRegister)
+    Button buttonRegister;
+    private
+    @BindView(R2.id.textViewSignin)
+    TextView textViewSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
     }
+
+    private void registerUser() {
+
+    }
+
 }
