f90524f20030b4ada5540f7e9241536ec5c635ea
jelloelks <kole.lleshaj@gmail.com>
2017-01-24 20:22:03 +0100

    Stuff

--- app/src/main/java/com/fatless/fatless/MainActivity.java
+++ app/src/main/java/com/fatless/fatless/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
