31f1076cb106d6a625d58d9f5289bbd3291b3e12
jelloelks <kole.lleshaj@gmail.com>
2017-01-24 11:43:14 +0100

    Facebook Stuff

--- app/src/main/java/com/fatless/fatless/MainActivity.java
+++ app/src/main/java/com/fatless/fatless/MainActivity.java
@@ -6,6 +6,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -28,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Butterknife bindings
         ButterKnife.bind(this);
 
-
+        //Facebook stuff
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
 
     }
 
