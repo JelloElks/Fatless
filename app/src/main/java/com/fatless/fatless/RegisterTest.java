b869d59601f63da56b6db441a4c6d6301a1fac59
jelloelks <kole.lleshaj@gmail.com>
2017-02-05 20:31:35 +0100

    Designad inloggning sida med fonts

---
+++ app/src/main/java/com/fatless/fatless/RegisterTest.java
@@ -0,0 +1,13 @@
+package com.fatless.fatless;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_test);
+    }
+}
