80e5dde84179619c09deb64fdb03f27dc5f26a78
Stefan Gustafsson <Stefangustaf@live.se>
2017-04-24 19:57:03 +0200

    Renamed LoggedInActivity to ProfileActivity Added  SharedPreferences to save data in profile Added a jpg penguin as default image

--- app/src/main/java/com/fatless/fatless/ProfileActivity.java
+++ app/src/main/java/com/fatless/fatless/ProfileActivity.java
@@ -1,39 +1,57 @@
 package com.fatless.fatless;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoggedInActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
-    private static final String TAG = LoggedInActivity.class.getName();
+    private static final String TAG = ProfileActivity.class.getName();
 
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
 
+    @BindView(R2.id.picture_in_frame)
+    ImageView picture_in_frame;
+    @BindView(R2.id.change_picture)
+    TextView change_picture;
+
+
     @BindView(R2.id.logout_button)
     Button logout_button;
-    @BindView(R2.id.user_email)
-    TextView user_email;
+    @BindView(R2.id.save_profile)
+    Button save_profile;
+
+    @BindView(R2.id.user_name)
+    EditText user_name;
+    @BindView(R2.id.user_age)
+    EditText user_age;
+    @BindView(R2.id.user_length)
+    EditText user_length;
+    @BindView(R2.id.user_weight)
+    EditText user_weight;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logged_in);
+        setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -45,21 +63,20 @@ public class LoggedInActivity extends AppCompatActivity {
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    prepareForm();
                 } else {
                     Log.d(TAG, "onAuthStateChanged:signed_out");
-                    startActivity(new Intent(LoggedInActivity.this, MainActivity.class));
+                    startActivity(new Intent(ProfileActivity.this, MainActivity.class));
                     finish();
                 }
 
             }
         };
-
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Name, email address, and profile photo Url
 
             String email = user.getEmail();
-            user_email.setText(email);
 
 
             // Check if user's email is verified
@@ -69,6 +86,14 @@ public class LoggedInActivity extends AppCompatActivity {
             // authenticate with your backend server, if you have one. Use
             // FirebaseUser.getToken() instead.
             String uid = user.getUid();
+
+            save_profile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    saveProfile();
+                    prepareForm();
+                }
+            });
 
             logout_button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -81,6 +106,36 @@ public class LoggedInActivity extends AppCompatActivity {
         }
 
     }
+
+    private void prepareForm() {
+
+        SharedPreferences preferences = getSharedPreferences("profileprefs", MODE_PRIVATE);
+
+        user_name.setText(preferences.getString("username", "..."));
+        user_age.setText(preferences.getString("userage", "..."));
+        user_length.setText(preferences.getString("userlength", "..."));
+        user_weight.setText(preferences.getString("userweight", "..."));
+    }
+
+    private void saveProfile() {
+        SharedPreferences preferences = getSharedPreferences("profileprefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        if (user_name != null) {
+            editor.putString("username", user_name.getText().toString());
+        }
+        if (user_age != null) {
+            editor.putString("userage", user_age.getText().toString());
+        }
+        if (user_length != null) {
+            editor.putString("userlength", user_length.getText().toString());
+        }
+        if (user_weight != null) {
+            editor.putString("userweight", user_weight.getText().toString());
+        }
+
+        editor.apply();
+    }
+
 
     @Override
     public void onStart() {
