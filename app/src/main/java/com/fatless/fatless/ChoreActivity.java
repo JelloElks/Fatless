32ffffd1d1757687b9ecd2fc133a9c85c238b243
Stefan Gustafsson <Stefangustaf@live.se>
2017-05-18 14:04:00 +0200

    can now start ChoreActivity from picked food also get value energyKcal

--- app/src/main/java/com/fatless/fatless/ChoreActivity.java
+++ app/src/main/java/com/fatless/fatless/ChoreActivity.java
@@ -1,9 +1,11 @@
 package com.fatless.fatless;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,7 +22,6 @@ public class ChoreActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-
     @BindView(R2.id.random_chore_button)
     Button random_chore_button;
     @BindView(R2.id.pick_chore_button)
@@ -29,6 +30,8 @@ public class ChoreActivity extends AppCompatActivity {
     @BindView(R2.id.chore_info_view)
     TextView chore_info_view;
 
+
+    private int energyKcal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +53,30 @@ public class ChoreActivity extends AppCompatActivity {
 
                 } else {
                     Log.d(TAG, "onAuthStateChanged:signed_out");
+                    startActivity(new Intent(ChoreActivity.this, LoginActivity.class));
+                    finish();
 
                 }
 
             }
         };
 
+        Intent intent = getIntent();
+        energyKcal = intent.getIntExtra("kcal", 0);
+
+        pick_chore_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        random_chore_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 
